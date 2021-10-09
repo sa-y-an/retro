d278725d36060a482c0607a975a5b62d22e99252
Sayan Mondal <sayan.ee.nitd@gmail.com>
2021-10-09 19:32:34 +0530

    Heroku LOcals

--- stress_detector/settings.py
+++ stress_detector/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://combat-stress.herokuapp.com/']
+ALLOWED_HOSTS = ['herokuapp.com']
 
 
 # Application definition
@@ -146,3 +146,6 @@ EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+django_heroku.settings(locals())
