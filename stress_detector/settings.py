b821438ec535d3d47e99c21edf62580c6ef2f2a4
Sayan Mondal <sayan.ee.nitd@gmail.com>
2021-10-09 19:30:22 +0530

    Procfile

--- stress_detector/settings.py
+++ stress_detector/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['herokuapp.com']
+ALLOWED_HOSTS = ['https://combat-stress.herokuapp.com/']
 
 
 # Application definition
