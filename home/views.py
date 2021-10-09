d5ca46466e4dafe5250d0e6ad4acae1ed8b8b157
Sayan Mondal <sayan.ee.nitd@gmail.com>
2021-10-09 23:31:01 +0530

    view ecg

--- home/views.py
+++ home/views.py
@@ -6,3 +6,9 @@ def home(request) :
 
 def eda(request) :
     return render(request, 'home/eda.html')
+
+def emg(request) :
+    return render(request, 'home/emg.html')
+
+def ecg(request) :
+    return render(request, 'home/ecg.html')
