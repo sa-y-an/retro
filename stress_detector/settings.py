df784712732ec8496a84b4eb48f10bfea4b37b3d
Sayan Mondal <sayan.ee.nitd@gmail.com>
2021-10-09 17:44:23 +0530

    System Template Done
    < Authored by (sayan.ee.nitd@gmail.com) >

--- stress_detector/settings.py
+++ stress_detector/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,10 +9,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6kmazml5fnpr&5%n%egazn)#yt4l89qr)%(oqbz!u9c1u)32og'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
